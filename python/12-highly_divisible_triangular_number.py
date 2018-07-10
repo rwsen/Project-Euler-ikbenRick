09c6be8341810e92419e4242648ed2eefbe7bf06
Rick <ikbenrick@gmail.com>
2018-07-10 08:53:24 +0200

    Added solution to problem 12

---
+++ python/12-highly_divisible_triangular_number.py
@@ -0,0 +1,31 @@
+# Project Euler 12
+import math
+
+def factors(number):
+    i = 1
+    limit = math.ceil(math.sqrt(number))
+    count = 0
+    while i < limit:
+        if number % i == 0:
+            count = count + 2
+        i = i + 1
+
+    if math.sqrt(number) % 1 == 0:
+        count += 1 
+
+    return count
+
+if __name__ == "__main__":
+    number = 1
+    triangle = number
+    best = 1
+
+    while True:
+        number = number + 1
+        triangle = triangle + number
+        count = factors(triangle)
+        if count > best:
+            best = count
+            print("triangle number %s has %s factors" % (triangle, count))
+        if count > 500:
+            break
