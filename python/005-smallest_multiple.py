13a4a919a4d79cb0a44d8d33b921acddc8de3a47
Rick <ikbenrick@gmail.com>
2018-09-29 17:58:46 +0200

    Naive solution for problem 5
    Works decent for finding the smallest multiple up to 16 as highest divisor, after that execution takes over 1 second

---
+++ python/005-smallest_multiple.py
@@ -0,0 +1,14 @@
+#PE005
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+solution = 0
+highest_number = 18
+
+while True:
+    solution += 1
+    for i in range (1,highest_number + 1):
+        if not (solution / i) % 1 == 0:
+            break
+    else:
+        # solution found
+        print(solution)
+        break
